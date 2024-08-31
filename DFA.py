85482f4eda6a0d5d2b841cbb677058effdfdec01
vranda0108 <2022bit015@sggs.ac.in>
2024-08-31 17:12:39 +0530

    DFA of string ending with 'b'.

---
+++ DFA.py
@@ -0,0 +1,35 @@
+def ends_with_b(text):
+    return "Accepted" if q0(text) else "Rejected"
+
+def q0(text):
+    if text[0] == 'a':
+        if len(text) == 1:
+            return False
+        else:
+            return q0(text[1:])
+    elif text[0] == 'b':
+        if len(text) == 1:
+            return True
+        else:
+            return q1(text[1:])
+    else:
+        return False
+
+def q1(text):
+    if text[0] == 'b':
+        if len(text) == 1:
+            return True
+        else:
+            return q1(text[1:])
+    elif text[0] == 'a':
+        if len(text) == 1:
+            return False
+        else:
+            return q0(text[1:])
+    else:
+        return False
+
+# Test the function
+print(ends_with_b("bababab"))
+print(ends_with_b("bababa"))
+
